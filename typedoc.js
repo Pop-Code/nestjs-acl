00130f4352bbcf51d8be082a9273e1b4f5f75286
Rmannn <alex.hermann@pop-code.com>
2021-03-29 15:44:19 +0200

    chore: update docs

--- typedoc.js
+++ typedoc.js
@@ -1,19 +1,9 @@
 module.exports = {
-    mode: 'modules',
     out: 'docs',
     exclude: ['**/test/**', '**/apollo-server-*/**'],
     theme: 'default',
     name: 'Nestjs ACL Documentation',
-    ignoreCompilerErrors: false,
     excludeExternals: true,
     excludePrivate: false,
-    excludeNotExported: false,
-    target: 'ES6',
-    moduleResolution: 'node',
-    preserveConstEnums: true,
-    stripInternal: false,
-    suppressExcessPropertyErrors: true,
-    suppressImplicitAnyIndexErrors: true,
-    module: 'commonjs',
     hideGenerator: true
 };
