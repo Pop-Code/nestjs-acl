6ccf2d3372eea1b5cc15caeda9daf8838e78d678
Rmannn <alex.hermann@pop-code.com>
2021-04-12 19:11:41 +0200

    fix: add missing constants to export

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
-export * from './module';
-export * from './interfaces';
-export * from './service';
-export * from './decorators';
+/**
+ * @module nestjs-acl
+ */
+
 export { AccessControl } from 'accesscontrol';
+export * from './constants';
+export * from './decorators';
+export * from './interfaces';
+export * from './module';
+export * from './service';
+
